e46c616dc9ea34ca4658fbdf2c40bf7e9de110d3
Mateu Adsuara <mateuadsuara@gmail.com>
2018-06-09 16:51:56 +0200

    Removes first bytes

--- src/EscapeCraft.java
+++ src/EscapeCraft.java
@@ -1,9 +1,14 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class EscapeCraft {
   public static byte[] readFile(String filePath) throws IOException {
     return Files.readAllBytes(Paths.get(filePath));
+  }
+
+  public static byte[] removeFirstBytes(byte[] content, int amount) {
+    return Arrays.copyOfRange(content, amount, content.length);
   }
 }
--- test/EscapeCraftTest.java
+++ test/EscapeCraftTest.java
@@ -11,4 +11,10 @@ public class EscapeCraftTest {
     byte[] content = EscapeCraft.readFile("/Users/m/Documents/Projects/escapecraft-kata/test/data/sample.bin");
     assertThat(content, is(new byte[]{0x00, 0x01, 0x02}));
   }
+
+  @Test
+  public void removesTheFirstBytes() {
+    byte[] bytes = EscapeCraft.removeFirstBytes(new byte[]{0x01, 0x02, 0x03}, 1);
+    assertThat(bytes, is(new byte[]{0x02, 0x03}));
+  }
 }
